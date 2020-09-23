83e122a2eba7922f2433f46ee440ef45dab1e57b
AmanG007 <amangupta.ag36@gmail.com>
2020-09-23 23:01:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-j2h24q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-j2h24q)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  onSubmit(form: NgForm) {
+    console.log('Your form data: ', form.value);
+  }
+  
+}
